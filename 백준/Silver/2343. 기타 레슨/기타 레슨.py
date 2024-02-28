edecbe703bde076926e7df1bcaef69b6ea341f4f
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-02-29 01:09:22 +0900

    [Silver I] Title: 기타 레슨, Time: 648 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+lecture_times=list(map(int,input().split()))
+
+left=max(lecture_times)
+right=sum(lecture_times)
+answer=0
+
+while left<=right:
+    cnt=1
+    total=0
+    mid=(left+right)//2
+
+    for i in range(n):
+        if total+lecture_times[i]>mid:
+            total=lecture_times[i]
+            cnt+=1
+        else:
+            total+=lecture_times[i]
+
+    if cnt>m:
+        left=mid+1
+    else:
+        right=mid-1
+
+print(left)
