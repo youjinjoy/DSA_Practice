7adfdec0152eaf6298a9151849fe86f8772fd44d
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-03-11 23:54:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+a=[0 for _ in range(n+1)]
+a[0]=0
+a[1]=1
+if n>1:
+  a[2]=2
+
+for i in range(3,n+1):
+  a[i]=a[i-1]+a[i-2]
+
+print(a[n]%10007)
