a3a69a8de3ed198b05b2c0b539dc3e654c5f14e9
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-03-29 16:32:40 +0900

    [Silver IV] Title: 차집합, Time: 1136 ms, Memory: 240068 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const inputPath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+const [[nA, nB], [...A], [...B]] = fs
+  .readFileSync(inputPath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+const setA = new Set(A);
+const setB = new Set(B);
+
+const union = setA.forEach((a) => {
+  if (setB.has(a)) setA.delete(a);
+});
+
+const answer = [...setA].sort((a, b) => a - b);
+console.log(answer.length);
+console.log(answer.join(" "));
