30c08396724c8bc7832cc062dee8d987d6353595
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-04-01 20:28:07 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 768 ms, Memory: 69336 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
@@ -2,17 +2,15 @@ const fs = require("fs");
 const inputPath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 const S = fs.readFileSync(inputPath).toString().trim();
 
-const hash = {};
+const partsSet = new Set();
 
 const n = S.length;
-// 1글자부터 n글자까지 window
-let window = 1;
-// n글자 크기일 때 멈춤
-while (window !== n) {
+
+for (let window = 1; window <= n; window++) {
   for (let i = 0; i <= n - window; i++) {
     const partS = S.slice(i, i + window);
-    hash[partS] = (hash[partS] || 0) + 1;
+    partsSet.add(partS);
   }
-  window += 1;
 }
-console.log(Object.keys(hash).length + 1); // n글자 케이스 +1 추가
+
+console.log(partsSet.size);
