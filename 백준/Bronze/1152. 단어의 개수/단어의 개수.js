5edfd8d14b9a7df74789774f18a02e9bb4391d12
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-04-16 14:11:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 148 ms, Memory: 17380 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -2,9 +2,8 @@ const fs = require("fs");
 const inputPath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 const s = fs.readFileSync(inputPath).toString().trim().split(" ");
 
-let answer = s.length;
-if (answer === 1 && s[0] === "") {
+if (s[0] === "") {
   console.log(0);
 } else {
-  console.log(answer);
+  console.log(s.length);
 }
