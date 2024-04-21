8f79fdb77cdb21a4d9526510c8220e18e3b66424
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-04-21 17:40:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 228 ms, Memory: 11920 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const inputPath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+const rl = readline.createInterface({
+  input: fs.createReadStream(inputPath),
+});
+
+let lineCount = 0;
+const board = [];
+let M = 0;
+let N = 0;
+rl.on("line", (line) => {
+  if (lineCount === 0) {
+    [N, M] = line.split(" ").map(Number);
+  } else {
+    board.push(line);
+  }
+  lineCount++;
+}).on("close", () => {
+  const black = Array.from({ length: 8 }, (x) => "");
+  const white = Array.from({ length: 8 }, (x) => "");
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((i % 2 === 0 && j % 2 === 0) || (i % 2 === 1 && j % 2 === 1)) {
+        black[i] += "B";
+        white[i] += "W";
+      } else {
+        black[i] += "W";
+        white[i] += "B";
+      }
+    }
+  }
+
+  let min = 2500;
+
+  for (let x = 8; x <= N; x++) {
+    for (let y = 8; y <= M; y++) {
+      let result = 0;
+
+      for (let i = x - 8; i < x; i++) {
+        for (let j = y - 8; j < y; j++) {
+          if (board[i][j] !== black[i - (x - 8)][j - (y - 8)]) {
+            result += 1;
+          }
+        }
+      }
+
+      min = Math.min(min, result);
+      result = 0;
+
+      for (let i = x - 8; i < x; i++) {
+        for (let j = y - 8; j < y; j++) {
+          if (board[i][j] !== white[i - (x - 8)][j - (y - 8)]) {
+            result += 1;
+          }
+        }
+      }
+
+      min = Math.min(min, result);
+    }
+  }
+
+  console.log(min);
+});
