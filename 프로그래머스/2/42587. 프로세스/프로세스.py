9eadcbeafd61af580ce9f77371271f2945cd9571
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-04-26 22:43:30 +0900

    Update 프로세스.py

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,18 @@
 from collections import deque
-import heapq
 
 def solution(priorities, location):
     h=[]
     q=deque()
-    for i in range(len(priorities)):
-        q.append((priorities[i],i))
-        heapq.heappush(h,-priorities[i])
     answer = 0
 
-    while len(h)!=0:
+    for i in range(len(priorities)):
+        q.append((priorities[i],i))
+        
+    priorities.sort()
+
+    while len(priorities)!=0:
         answer+=1
-        m=-heapq.heappop(h)
+        m=priorities.pop()
         while q[0][0]<m:
             q.append(q.popleft())
         if q.popleft()[1]==location:
