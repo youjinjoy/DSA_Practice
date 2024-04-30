80216668fe147c659f4b8853b1cafee045755665
Youjin Jo <youjinjoy@gmail.com>
2024-04-30 16:39:23 +0900

    다리를 지나는 트럭 재제출

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -5,21 +5,15 @@ def solution(bridge_length, weight, truck_weights):
     time = 0
     q = deque([0 for _ in range(bridge_length)])
     for truck in truck_weights:
-        if w+truck > weight:
-            while True:
-                w -= q.popleft()
-                time+=1
-                if w+truck > weight:
-                    q.append(0)
-                else:
-                    q.append(truck)
-                    w += truck
-                    break
-        else:
+        while True:
             w -= q.popleft()
-            q.append(truck)
-            w += truck
-            time += 1
+            time+=1
+            if w+truck > weight:
+                q.append(0)
+            else:
+                q.append(truck)
+                w += truck
+                break
 
     time += bridge_length
     
