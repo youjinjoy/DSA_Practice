91e280451e4090d2337a52de7322fba2dcfdee11
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-05-05 12:58:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,23 @@
+import heapq as hq
+
+def solution(jobs):
+    heap = []
+    count = 0
+    answer = 0
+    b= -1
+    
+    jobs.sort()
+    time = jobs[0][0]
+    while count < len(jobs):
+        for s,t in jobs:
+            if b < s <= time:
+                hq.heappush(heap, [t,s])
+        if heap:
+            count += 1
+            [t,s] = hq.heappop(heap)
+            b = time
+            time += t
+            answer += time - s
+        else:
+            time += 1
+    return answer//len(jobs)
