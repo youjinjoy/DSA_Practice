c55aa83b92beaff29548abb7c3ab1121f0e72331
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-05-14 11:31:08 +0900

    [Gold V] Title: 괄호의 값, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -4,7 +4,8 @@ input = sys.stdin.readline
 def solution():
     bracket=input().strip()
     stack = []
-    numbers = []
+    # numbers 배열 없앤 버전
+    
     for i in bracket:
         ## 여는 기호일 때
         if i == '(' or i == '[':
@@ -12,45 +13,44 @@ def solution():
         ## 닫는 기호일 때
         # )
         elif i == ')':
+            temp = 0
             while True:
                 if not stack:
                     return 0
-                x = stack.pop()
-                if x=='[':
-                    return 0
-                elif x == '(':
-                    if numbers:
-                        stack.append(sum(numbers)*2)
-                        numbers = []
-                    else: # number = []
+                top = stack.pop()
+                if top == '(':
+                    if temp == 0:
                         stack.append(2)
+                    else:
+                        stack.append(2 * temp)
                     break
-                else: # x가 숫자면
-                    numbers.append(x)
+                elif top == '[':
+                    return 0
+                else: # top이 숫자일 때
+                    temp += top
         # ]
         elif i == ']':
+            temp = 0
             while True:
                 if not stack:
                     return 0
-                x = stack.pop()
-                if x=='(':
-                    return 0
-                elif x == '[':
-                    if numbers:
-                        stack.append(sum(numbers)*3)
-                        numbers = []
-                    else: # numbers = []
+                top = stack.pop()
+                if top == '[':
+                    if temp == 0:
                         stack.append(3)
+                    else:
+                        stack.append(3 * temp)
                     break
-                else: # x가 숫자면
-                    numbers.append(x)
+                elif top == '(':
+                    return 0
+                else: # top이 숫자일 때
+                    temp += top
 
     answer = 0
     for s in stack:
-        if type(s) == int:
-            answer+=s
-        else:
+        if type(s) != int:
             return 0
+        answer += s
     return answer
 
 print(solution())
