dbc13619f484f9dade0ad29a6afb355e9a767478
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-06-01 14:42:50 +0900

    [Silver II] Title: N과 M (9), Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -8,17 +8,16 @@ history = []
 
 def dfs(depth,current):
   if depth == M:
-    current_string = ' '.join(map(str,current))
-    if current_string not in history:
-      print(current_string)
-      history.append(current_string)
+    print(' '.join(map(str,current)))
     return
 
+  last = -1
   for i in range(N):
-    if not used[i]:
+    if not used[i] and arr[i] != last:
       used[i] = True
       dfs(depth+1,current+[arr[i]])
       used[i] = False
+      last = arr[i]
       
 arr.sort()
 dfs(0,[])
