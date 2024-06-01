30ecacd645baba4cae32a08185d6f8eccff145f0
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-06-01 13:14:09 +0900

    [Silver III] Title: N과 M (4), Time: 72 ms, Memory: 34000 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,M = map(int,input().split(' '))
+
+def dfs(depth,current,start):
+  if depth == M:
+    print(' '.join(map(str,current)))
+    return
+
+  for i in range(start,N+1):
+    dfs(depth+1,current+[i],i)
+
+dfs(0,[],1)
