b16d0ce99d2ab5abed4503ae17b10e1951078937
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-06-02 14:51:59 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 136.33 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -2,38 +2,31 @@ def solution(rows, columns, queries):
     answer = []
     
     # 행렬 만들기
-    maps = [[0 for _ in range(columns+1)]]
-    maps += [[i*columns + j for j in range(columns+1)] for i in range(rows)]
-    for i in range(rows+1):
-        maps[i][0] = 0
+    maps = [[i*columns + j + 1 for j in range(columns)] for i in range(rows)]
     
     # 회전 적용
-    for query in queries:
-        x1,y1,x2,y2 = query
-        arr = []
-        for j in range(y1,y2):
-            arr.append(maps[x1][j])
+    for x1,y1,x2,y2 in queries:
+        x1,y1,x2,y2 = x1-1,y1-1,x2-1,y2-1 # 인덱스 조정
+        temp = maps[x1][y1]
+        m = min(10000,temp)
+        
         for i in range(x1,x2):
-            arr.append(maps[i][y2])
-        for j in range(y2,y1,-1):
-            arr.append(maps[x2][j])
+            maps[i][y1] = maps[i+1][y1]
+            m = min(m, maps[i][y1])
+            
+        for j in range(y1,y2):
+            maps[x2][j] = maps[x2][j+1]
+            m = min(m, maps[x2][j])
+            
         for i in range(x2,x1,-1):
-            arr.append(maps[i][y1])
-        answer.append(min(arr))
-        new_arr = [arr[-1]]+arr[:-1]
+            maps[i][y2] = maps[i-1][y2]
+            m = min(m, maps[i][y2])
+            
+        for j in range(y2,y1,-1):
+            maps[x1][j] = maps[x1][j-1]
+            m = min(m, maps[x1][j])
 
-        k=0
-        for j in range(y1,y2):
-            maps[x1][j] = new_arr[k]
-            k += 1
-        for i in range(x1,x2):
-            maps[i][y2] = new_arr[k]
-            k += 1
-        for j in range(y2,y1,-1):
-            maps[x2][j] = new_arr[k]
-            k += 1
-        for i in range(x2,x1,-1):
-            maps[i][y1] = new_arr[k]
-            k += 1
+        maps[x1][y1+1] = temp
+        answer.append(m)
         
     return answer
