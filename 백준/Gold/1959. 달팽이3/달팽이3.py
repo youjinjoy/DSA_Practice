23cfbf3f679de1e1d940fe8853824bb0a967f059
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-06-03 20:05:48 +0900

    [Gold III] Title: 달팽이3, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1959. 달팽이3/달팽이3.py
+++ 백준/Gold/1959. 달팽이3/달팽이3.py
@@ -4,10 +4,20 @@ input = sys.stdin.readline
 X,Y = map(int,input().split(' '))
 
 K = 0
-while True:
-  if X-2*K <=2 or Y-2*K <= 2:
+left = 0
+right = 2_100_000_001
+
+while left<=right:
+  mid = (left+right)//2
+  sx,sy = X-2*mid, Y-2*mid
+  
+  if sx >2 and sy >2:
+    left = mid+1
+  elif sx<=0 or sy<=0:
+    right = mid-1
+  else:
     break
-  K += 1
+K = mid
 
 result = 0
 x,y = 0,0
@@ -17,7 +27,7 @@ if X>Y: # 세로가 가로보다 클 때
     x,y = 2+K,1+K
   else: # 가로가 홀수일 때
     x,y = X-K,Y-K
-else: # 세로가 가로보다 작거나 같을 때
+else: # 가로가 세로보다 작거나 클 때
   result = 2*(X-1)
   if X%2 == 0 : # 세로가 짝수일 때
     x,y = X-K,1+K
