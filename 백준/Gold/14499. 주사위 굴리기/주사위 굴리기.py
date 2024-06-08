4916aaa7f86c4f61d3aa96d7ea22babc23fb4fca
youjin <123347724+youjinjoy@users.noreply.github.com>
2024-06-08 16:35:49 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -5,47 +5,28 @@ N,M,x,y,K = map(int, input().split(' '))
 maps = [list(map(int, input().split(' '))) for _ in range(N)]
 operations = list(map(int, input().split(' ')))
 
-ewns = [(0,1),(0,-1),(-1,0),(1,0)] # 동 서 북 남
+dK = [None,(0,1),(0,-1),(-1,0),(1,0)] # 동 서 북 남
 
-# 밑면 기준. X.
-bottom = [
-  None,
-  # E W N S 순
-  [3,4,5,2], # 밑이 1인 경우
-  [3,4,1,6], # 밑이 2인 경우
-  [1,6,2,5], # 밑이 3인 경우
-  [6,1,2,5], # 밑이 4인 경우
-  [3,4,6,1], # 밑이 5인 경우
-  [3,4,2,5], # 밑이 6인 경우
-]
-
-# 1: 위, 2: 뒤, 3: 앞, 4: 왼쪽, 5: 오른쪽, 6: 밑
-dice = [0 for _ in range(7)]
+T = 0
+B = 0
+side = [None, 0, 0, 0, 0]
+# 1: 왼쪽, 2: 오른쪽, 3: 앞, 4: 뒤
 
 for op in operations:
-  dx,dy = ewns[op-1]
-  nx,ny = x+dx,y+dy
-  if 0<=nx<N and 0<=ny<M:
-    
-    # 1: 위, 2: 뒤, 3: 앞, 4: 왼쪽, 5: 오른쪽, 6: 밑
-    if op == 1:
-      # 동쪽
-      dice[1],dice[3],dice[6],dice[4] = dice[4],dice[1],dice[3],dice[6]
-    elif op == 2:
-      # 서쪽
-      dice[1],dice[3],dice[6],dice[4] = dice[3],dice[6],dice[4],dice[1]
-    elif op == 3:
-      # 북쪽
-      dice[6],dice[2],dice[1],dice[5] = dice[2],dice[1],dice[5],dice[6]
-    else: # op ==4:
-      # 남쪽
-      dice[6],dice[2],dice[1],dice[5] = dice[5],dice[6],dice[2],dice[1]
+  nx,ny = x+dK[op][0],y+dK[op][1]
+  if nx in (-1,N):
+    continue
+  if ny in (-1,M):
+    continue
 
-    if maps[nx][ny] == 0:
-      maps[nx][ny] = dice[6]
-    else:
-      dice[6] = maps[nx][ny]
-      maps[nx][ny] = 0
+  aux = op+1 if op%2 == 1 else op-1
+  T,B,side[op],side[aux] = side[op],side[aux],B,T
+  print(T)
 
-    x,y = nx,ny
-    print(dice[1])
+  if maps[nx][ny] == 0:
+    maps[nx][ny] = B
+  else:
+    B = maps[nx][ny]
+    maps[nx][ny] = 0
+
+  x,y = x+dK[op][0], y+dK[op][1]
